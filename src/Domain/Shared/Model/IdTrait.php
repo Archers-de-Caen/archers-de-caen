bf03801ae4d1bb55231db8009642becadf675b53
Damien Hebert <damien@ganapati.fr>
2022-04-10 00:58:40 +0200

    req symfony/uid

--- src/Domain/Shared/Model/IdTrait.php
+++ src/Domain/Shared/Model/IdTrait.php
@@ -6,15 +6,18 @@ namespace App\Domain\Shared\Model;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
 
 trait IdTrait
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: Types::INTEGER)]
-    private ?int $id;
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\Column(type: 'uuid', unique: true)]
+    #[ORM\CustomIdGenerator(class: "doctrine.uuid_generator")]
+    private ?Uuid $id;
 
-    public function getId(): ?int
+    public function getId(): ?Uuid
     {
         return $this->id;
     }
