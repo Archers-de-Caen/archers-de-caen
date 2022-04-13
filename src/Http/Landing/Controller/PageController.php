2361332a047ff919a2d2aa78893a1f0d595153c1
Damien Hebert <damien-1998@live.fr>
2022-04-14 00:54:35 +0200

    wip: paginator

--- src/Http/Landing/Controller/PageController.php
+++ src/Http/Landing/Controller/PageController.php
@@ -7,19 +7,25 @@ namespace App\Http\Landing\Controller;
 use App\Domain\Cms\Config\Category;
 use App\Domain\Cms\Model\Page;
 use App\Domain\Cms\Repository\PageRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
     #[Route('/actualites', name: 'landing_actualities')]
-    public function actualities(PageRepository $pageRepository): Response
+    public function actualities(Request $request, PageRepository $pageRepository): Response
     {
         return $this->render('/landing/actualities/actualities.html.twig', [
-            'actualities' => $pageRepository->findBy(
-                ['category' => Category::ACTUALITY->value],
-                ['createdAt' => 'DESC']
+            'actualities' => new Paginator(
+                $pageRepository
+                    ->createQueryBuilder('p')
+                    ->where("p.category = '" . Category::ACTUALITY->value . "'")
+                    ->orderBy('p.createdAt', 'DESC')
+                    ->setFirstResult((int) $request->query->get('page') ?: 0)
+                    ->setMaxResults((int) (24 * ($request->query->get('page') ?: 1))) // 24, car sur un écran 1080p la dernière ligne est complete
             ),
         ]);
     }
