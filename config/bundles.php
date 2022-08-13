fbc3b0f27c0b38cdaebc59c883651c298bf3c187
Damien Hebert <damien-1998@live.fr>
2022-08-13 21:17:25 +0200

    update: composer + yarn + config file
    update: composer symfony 6.0 to 6.1
    update: recipe doctrine
    update: recipe sentry
    update: recipe symfony/flex
    update: symfony/mailer
    update: symfony/routing
    update: symfony/security-bundle
    update: symfony/web-profiler-bundle
    update: symfony/webpack
    update: yarn
    clean code: remove useless config file
    clean code: suite

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
