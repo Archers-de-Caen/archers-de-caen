d49ff0400553faa02c9458e4edb7934e92b9e4d6
Damien Hebert <damien-1998@live.fr>
2022-10-01 22:38:39 +0200

    feat: Ajout de liip imagine pour reduire la résolution des images de la galerie

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
