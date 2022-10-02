5046d8437f77c5a59262698f20b8b6beb32fae9f
Damien Hebert <damien-1998@live.fr>
2022-10-02 14:19:19 +0200

    fix: ajout de embedly pour afficher les media ajouter par ckeditor

--- assets/app.js
+++ assets/app.js
@@ -18,6 +18,7 @@ import './js/copy'
 import './js/flash'
 import './js/iframe-fit-content'
 import './js/display'
+import './js/embedly'
 
 // feather-icons a plugin for the open-source icon set, show https://feathericons.com/ for complete icons list
 const feather = require('feather-icons')
