6b3feda347bb03c4df3cb4815dc22016e25234a1
Damien Hebert <damien-1998@live.fr>
2022-12-24 16:23:30 +0100

    feat: prendre une licence

---
+++ src/Domain/Cms/Form/Data/TakeLicenseForm.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Cms\Form\Data;
+
+use App\Domain\Cms\Form\Data\Element\ButtonForm;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Appelé depuis App\Http\Admin\Controller\DataCrudController::createEditFormBuilder.
+ */
+class TakeLicenseForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('license_form', UrlType::class, [
+                'label' => 'Formulaire de demande de création de licence',
+            ])
+            ->add('assurance', UrlType::class, [
+                'label' => 'Notice d\'assurance',
+            ])
+            ->add('health', UrlType::class, [
+                'label' => 'Questionnaire santé',
+            ])
+        ;
+    }
+}
--- src/Http/Landing/Controller/LicenseController.php
+++ src/Http/Landing/Controller/LicenseController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Landing\Controller;
 
+use App\Domain\Cms\Repository\DataRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,10 @@ class LicenseController extends AbstractController
     public const ROUTE_LANDING_LICENSE_NEW = 'landing_license_new';
 
     #[Route('/prendre-une-licence', name: self::ROUTE_LANDING_LICENSE_NEW)]
-    public function index(): Response
+    public function index(DataRepository $dataRepository): Response
     {
-        return $this->render('/landing/license/new.html.twig');
+        return $this->render('/landing/license/new.html.twig', [
+            'documents' => $dataRepository->findOneBy(['code' => 'TAKE_LICENSE'])?->getContent(),
+        ]);
     }
 }
