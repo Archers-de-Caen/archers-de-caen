edfd5256b1e155dd22d32a90eaf5ce8f8b50bffc
Damien Hebert <damien-1998@live.fr>
2022-12-24 14:22:36 +0100

    feat: Boutique

--- assets/js/iframe-fit-content.js
+++ assets/js/iframe-fit-content.js
@@ -1,20 +1,38 @@
+function fitHeight(element) {
+    // +50 je ne sais pas d'où ça sort, mais ça marche
+
+    if (element.contentDocument) {
+        element.height = element.contentDocument.body.scrollHeight + 50 + 'px'
+    }
+}
+
+function fitWidth(element) {
+    if (element.contentDocument.body) {
+        element.width = element.contentDocument.body.scrollWidth + 'px'
+    }
+}
+
+function fit(element) {
+    fitHeight(element)
+    fitWidth(element)
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.fit-height-content').forEach((element) => {
-        element.addEventListener('load', () => {
-            element.height = element.contentDocument.body.scrollHeight + 50 + 'px' // +50 je ne sais pas d'où ça sort, mais ça marche
-        })
+        element.addEventListener('load', () => fitHeight(element))
     })
 
     document.querySelectorAll('.fit-width-content').forEach((element) => {
-        element.addEventListener('load', () => {
-            element.width = element.contentDocument.body.scrollWidth + 'px'
-        })
+        element.addEventListener('load', () => fitWidth(element))
     })
 
     document.querySelectorAll('.fit-content').forEach((element) => {
-        element.addEventListener('load', () => {
-            element.height = element.contentDocument.body.scrollHeight + 50 + 'px' // +50 je ne sais pas d'où ça sort, mais ça marche
-            element.width = element.contentDocument.body.scrollWidth + 'px'
-        })
+        element.addEventListener('load', () => fit(element))
     })
+})
+
+window.addEventListener('resize', () => {
+    document.querySelectorAll('.fit-height-content').forEach((element) => fitHeight(element))
+    document.querySelectorAll('.fit-width-content').forEach((element) => fitWidth(element))
+    document.querySelectorAll('.fit-content').forEach((element) => fit(element))
 })
