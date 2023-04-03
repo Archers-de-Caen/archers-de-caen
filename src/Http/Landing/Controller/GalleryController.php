c465b95c044648b7934bb6fbc50c88e757dd8e6b
Damien Hebert <damien-1998@live.fr>
2023-04-04 00:03:39 +0200

    enhancement(galleries): Ajout d'une pagination pour les photos

--- src/Http/Landing/Controller/GalleryController.php
+++ src/Http/Landing/Controller/GalleryController.php
@@ -6,7 +6,11 @@ namespace App\Http\Landing\Controller;
 
 use App\Domain\Cms\Model\Gallery;
 use App\Domain\Cms\Repository\GalleryRepository;
+use App\Domain\File\Repository\PhotoRepository;
+use App\Helper\PaginatorHelper;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,18 +20,42 @@ class GalleryController extends AbstractController
     public const ROUTE_LANDING_GALLERY = 'landing_gallery';
 
     #[Route('/galeries', name: self::ROUTE_LANDING_GALLERIES)]
-    public function galleries(GalleryRepository $galleryRepository): Response
+    public function galleries(Request $request, GalleryRepository $galleryRepository): Response
     {
+        $currentPage = ((int) $request->query->get('page') ?: 1) - 1;
+        $elementByPage = 16;
+
+        $galleries = new Paginator(
+            $galleryRepository->createQueryBuilder('gallery')
+                ->orderBy('gallery.createdAt', 'DESC')
+                ->setFirstResult($currentPage * $elementByPage)
+                ->setMaxResults($elementByPage) // 24, car sur un écran 1080p la dernière ligne est complete
+        );
+
         return $this->render('/landing/galleries/galleries.html.twig', [
-            'galleries' => $galleryRepository->findBy([], ['createdAt' => 'DESC']),
+            'galleries' => $galleries,
+            'paginator' => PaginatorHelper::pagination($currentPage + 1, (int) ceil($galleries->count() / $elementByPage)),
         ]);
     }
 
     #[Route('/galerie/{slug}', name: self::ROUTE_LANDING_GALLERY)]
-    public function gallery(Gallery $gallery): Response
+    public function gallery(Request $request, Gallery $gallery, PhotoRepository $photoRepository): Response
     {
+        $currentPage = ((int) $request->query->get('page') ?: 1) - 1;
+        $elementByPage = 16;
+
+        $photos = new Paginator(
+            $photoRepository->createQueryBuilder('photo')
+                ->where('photo.gallery = :gallery')
+                ->setParameter('gallery', $gallery->getId(), 'uuid')
+                ->setFirstResult($currentPage * $elementByPage)
+                ->setMaxResults($elementByPage) // 24, car sur un écran 1080p la dernière ligne est complete);
+        );
+
         return $this->render('/landing/galleries/gallery.html.twig', [
             'gallery' => $gallery,
+            'photos' => $photos,
+            'paginator' => PaginatorHelper::pagination($currentPage + 1, (int) ceil($photos->count() / $elementByPage)),
         ]);
     }
 }
