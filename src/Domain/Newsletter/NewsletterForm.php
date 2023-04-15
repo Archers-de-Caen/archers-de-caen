de4e0081035a5e0f50d6474ff337bcb1250f809b
Damien Hebert <damien-1998@live.fr>
2023-04-15 14:39:21 +0200

    hotfix(newsletter): Ajout lien de désinscription

--- src/Domain/Newsletter/NewsletterForm.php
+++ src/Domain/Newsletter/NewsletterForm.php
@@ -42,7 +42,7 @@ class NewsletterForm extends AbstractType
                 ],
             ])
             ->add('submit', SubmitType::class, [
-                'label' => "S'inscrire",
+                'label' => "Enregistrer",
             ])
         ;
     }
