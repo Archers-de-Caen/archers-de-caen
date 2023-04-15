e6c604d142d6095f1e30d1bc10f3829f507514fb
Damien Hebert <damien-1998@live.fr>
2023-04-15 14:58:17 +0200

    fix import

--- src/Http/Landing/Controller/ShopController.php
+++ src/Http/Landing/Controller/ShopController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Landing\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
