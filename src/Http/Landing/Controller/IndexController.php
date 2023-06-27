5727eaa3ba3d27a6545f912de92dca27cda998c7
Damien Hebert <damien-1998@live.fr>
2023-06-27 21:39:06 +0200

    enhancement(header): Refonte du menu

--- src/Http/Landing/Controller/IndexController.php
+++ src/Http/Landing/Controller/IndexController.php
@@ -6,6 +6,7 @@ namespace App\Http\Landing\Controller;
 
 use App\Domain\Cms\Config\Category;
 use App\Domain\Cms\Config\Status;
+use App\Domain\Cms\Model\Page;
 use App\Domain\Cms\Repository\DataRepository;
 use App\Domain\Cms\Repository\GalleryRepository;
 use App\Domain\Cms\Repository\PageRepository;
@@ -26,55 +27,38 @@ class IndexController extends AbstractController
 {
     public const ROUTE = 'landing_index';
 
-    public function __invoke(PageRepository $pageRepository, DataRepository $dataRepository, GalleryRepository $galleryRepository): Response
+    public function __invoke(PageRepository $pageRepository, DataRepository $dataRepository): Response
     {
         $actualityLocked = null;
         if ($actualityLockedData = $dataRepository->findOneBy(['code' => 'INDEX_ACTUALITY_LOCKED'])?->getContent()) {
             $actualityLocked = $pageRepository->findOneBy(['slug' => $actualityLockedData[array_key_first($actualityLockedData)]]);
         }
 
-        $actualities = $pageRepository->findBy(
-            [
-                'category' => Category::ACTUALITY->value,
-                'status' => Status::PUBLISH,
-            ],
-            ['createdAt' => 'DESC'],
-            $actualityLocked ? 3 : 4
-        );
+        /** @var array<Page> $actualities */
+        $actualities = $pageRepository->createQueryBuilder('p')
+            ->select('p', 'image')
+            ->leftJoin('p.image', 'image')
+
+            ->where('p.category = :category')
+            ->setParameter('category', Category::ACTUALITY->value)
+
+            ->andWhere('p.status = :status')
+            ->setParameter('status', Status::PUBLISH)
+
+            ->orderBy('p.createdAt', 'DESC')
+
+            ->setMaxResults($actualityLocked ? 3 : 4)
+
+            ->getQuery()
+            ->getResult()
+        ;
 
         if ($actualityLocked) {
             $actualities[] = $actualityLocked;
         }
 
-        $pages = $pageRepository->findTagNameBy('sport');
-
-        $pagesSortByTags = [];
-        foreach ($pages as $page) {
-            $tagsName = [];
-            foreach ($page->getTags() as $tag) {
-                if ($tag->getName() && 'sport' !== strtolower($tag->getName())) {
-                    $tagsName[] = $tag->getName();
-                }
-            }
-
-            if (!\count($tagsName)) {
-                $tagsName[] = 'no-category';
-            }
-
-            if (!isset($pagesSortByTags[$tagsName[0]])) {
-                $pagesSortByTags[$tagsName[0]] = [];
-            }
-
-            $pagesSortByTags[$tagsName[0]][] = $page;
-        }
-
         return $this->render('/landing/index/index.html.twig', [
             'actualities' => $actualities,
-            'header' => [
-                'actualities' => $pageRepository->findBy(['category' => Category::ACTUALITY->value, 'status' => Status::PUBLISH], ['createdAt' => 'DESC'], 10),
-                'galleries' => $galleryRepository->findBy([], limit: 8),
-                'sport' => $pagesSortByTags,
-            ],
             'contents' => $dataRepository->findOneBy(['code' => 'INDEX_PAGE_ELEMENT'])?->getContent(),
             'partners' => $dataRepository->findOneBy(['code' => 'PARTNER'])?->getContent(),
         ]);
