0703459d713242c052b58df5488140e30f91c078
Damien Hebert <damien-1998@live.fr>
2023-06-29 22:12:58 +0200

    feat(popup): Ajout d'une popup configurable

---
+++ src/Domain/Cms/Form/Data/PopupForm.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Cms\Form\Data;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Appelé depuis App\Http\Admin\Controller\DataCrudController::createEditFormBuilder.
+ */
+class PopupForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])
+            ->add('image', UrlType::class, [
+                'label' => 'Lien image',
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu',
+            ])
+            ->add('enable', CheckboxType::class, [
+                'label' => 'Activé ?',
+            ])
+        ;
+    }
+}
