68f69a96439da5ef69108c1186003c2012264628
Damien HEBERT <damien.hebert.98@gmail.com>
2023-10-26 11:35:41 +0200

    wip

--- migrations/Version20230404195800.php
+++ migrations/Version20230404195800.php
@@ -16,7 +16,7 @@ final class Version20230404195800 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql("CREATE TABLE newsletter (id BINARY(16) NOT NULL COMMENT '(DC2Type:uuid)', emails LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:simple_array)\', context LONGTEXT NOT NULL, type VARCHAR(191) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB");
+        $this->addSql("CREATE TABLE newsletter (id BINARY(16) NOT NULL COMMENT '(DC2Type:uuid)', emails LONGTEXT DEFAULT NULL COMMENT '(DC2Type:simple_array)', context LONGTEXT NOT NULL, type VARCHAR(191) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB");
         $this->addSql("ALTER TABLE newsletter CHANGE context context LONGTEXT NOT NULL COMMENT '(DC2Type:json)'");
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE archer ADD newsletters LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:simple_array)\'');
