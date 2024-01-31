a1002b5507e6acbe5c7f24ba6edbc589f6a4c152
HEBERT Damien <damien-1998@live.fr>
2024-01-31 14:11:34 +0100

    chore: add rector

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    // register single rule
+    $rectorConfig->rule(TypedPropertyFromStrictConstructorRector::class);
+
+    // here we can define, what sets of rules will be applied
+    // tip: use "SetList" class to autocomplete sets with your IDE
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::PHP_83,
+        SetList::PRIVATIZATION,
+        SetList::INSTANCEOF,
+        SetList::TYPE_DECLARATION,
+        SetList::EARLY_RETURN,
+        SetList::STRICT_BOOLEANS,
+        SetList::DEAD_CODE,
+    ]);
+};
