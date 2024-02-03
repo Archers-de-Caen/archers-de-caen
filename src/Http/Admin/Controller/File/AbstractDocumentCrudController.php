5a891cc0e7e44c3faf85c232a6108ae0290c1a30
HEBERT Damien <damien-1998@live.fr>
2024-02-03 22:57:32 +0100

    fix: rector

--- src/Http/Admin/Controller/File/AbstractDocumentCrudController.php
+++ src/Http/Admin/Controller/File/AbstractDocumentCrudController.php
@@ -4,17 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Admin\Controller\File;
 
-use App\Domain\File\Config\DocumentType;
 use App\Domain\File\Model\Document;
-use Doctrine\ORM\QueryBuilder;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
