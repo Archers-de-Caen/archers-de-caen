88987b5179a48234b0d1501dc1ad47d324f09f61
HEBERT Damien <damien-1998@live.fr>
2024-07-03 22:48:13 +0200

    enhancement(style-guide): add logo, font and logo colors

--- src/Http/Landing/Controller/Page/StyleGuideController.php
+++ src/Http/Landing/Controller/Page/StyleGuideController.php
@@ -7,9 +7,11 @@ namespace App\Http\Landing\Controller\Page;
 use App\Domain\Archer\Model\Archer;
 use App\Helper\PaginatorHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[AsController]
@@ -22,9 +24,18 @@ final class StyleGuideController extends AbstractController
 {
     public const string ROUTE = 'landing_style_guide';
 
-    public function __invoke(Request $request): Response
-    {
-        if (!$this->isGranted(Archer::ROLE_DEVELOPER) && 'dev' !== $request->server->get('APP_ENV')) {
+    public function __invoke(
+        Request $request,
+        #[Autowire(env: 'APP_PASSWORD_STYLE_GUIDE')]
+        string $password,
+        #[MapQueryParameter('password')]
+        ?string $userPassword = null,
+    ): Response {
+        if (
+            !$this->isGranted(Archer::ROLE_DEVELOPER)
+            && 'dev' !== $request->server->get('APP_ENV')
+            && $password !== $userPassword
+        ) {
             throw $this->createAccessDeniedException();
         }
 
