f45d2aed38209c0b9faa3a04fb348e71ba2cf21f
am dzaien <damien-1998@live.fr>
2024-10-23 14:41:13 +0200

    refactor(style-guide): remove password on style-guide page

--- src/Http/Landing/Controller/Page/StyleGuideController.php
+++ src/Http/Landing/Controller/Page/StyleGuideController.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Landing\Controller\Page;
 
-use App\Domain\Archer\Model\Archer;
 use App\Helper\PaginatorHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[AsController]
@@ -24,21 +21,8 @@ final class StyleGuideController extends AbstractController
 {
     public const string ROUTE = 'landing_style_guide';
 
-    public function __invoke(
-        Request $request,
-        #[Autowire(env: 'APP_PASSWORD_STYLE_GUIDE')]
-        string $password,
-        #[MapQueryParameter('password')]
-        ?string $userPassword = null,
-    ): Response {
-        if (
-            !$this->isGranted(Archer::ROLE_DEVELOPER)
-            && 'dev' !== $request->server->get('APP_ENV')
-            && $password !== $userPassword
-        ) {
-            throw $this->createAccessDeniedException();
-        }
-
+    public function __invoke(Request $request): Response
+    {
         return $this->render('/landing/style-guide/style-guide.html.twig', [
             'paginator' => PaginatorHelper::pagination((int) ($request->query->get('page') ?: 1), 100),
         ]);
