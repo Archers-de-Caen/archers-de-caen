9e6e0f52649f3cd3525a763066ee446ce3e5bc3e
Damien Hebert <damien@kanta.fr>
2024-10-30 13:25:33 +0100

    feat(gallery): publish gallery without send newsletter

--- src/Http/Admin/Controller/GalleryCrudController.php
+++ src/Http/Admin/Controller/GalleryCrudController.php
@@ -36,7 +36,8 @@ final class GalleryCrudController extends AbstractCrudController
 {
     public function __construct(
         private readonly UrlGeneratorInterface $urlGenerator,
-        private readonly MessageBusInterface $bus,
+        private readonly MessageBusInterface $messageBus,
+        private readonly EntityManagerInterface $em,
     ) {
     }
 
@@ -67,6 +68,12 @@ final class GalleryCrudController extends AbstractCrudController
             ->displayIf(static fn (Gallery $gallery): bool => Status::DRAFT === $gallery->getStatus())
         ;
 
+        $publishWithoutSendNewsletter = Action::new('publishWithoutSendNewsletter')
+            ->setLabel('Publier sans envoyer la newsletter')
+            ->linkToCrudAction('publishWithoutSendNewsletter')
+            ->displayIf(static fn (Gallery $gallery): bool => Status::DRAFT === $gallery->getStatus())
+        ;
+
         $publicLink = Action::new('public-link')
             ->setLabel('Lien public')
             ->linkToUrl(function (Gallery $gallery): string {
@@ -78,6 +85,7 @@ final class GalleryCrudController extends AbstractCrudController
 
         return $actions
             ->add(Crud::PAGE_INDEX, $publish)
+            ->add(Crud::PAGE_INDEX, $publishWithoutSendNewsletter)
             ->add(Crud::PAGE_INDEX, $publicLink);
     }
 
@@ -147,32 +155,39 @@ final class GalleryCrudController extends AbstractCrudController
     private function dispatchCache(Gallery $entityInstance): void
     {
         if ($entityInstance->getMainPhoto() instanceof Photo && $entityInstance->getMainPhoto()->getImageName()) {
-            $this->bus->dispatch(new CacheResolveMessage($entityInstance->getMainPhoto()->getImageName()));
+            $this->messageBus->dispatch(new CacheResolveMessage($entityInstance->getMainPhoto()->getImageName()));
         }
 
-        $this->bus->dispatch(new CacheResolveMessage($entityInstance->getPhotos()->map(static fn ($photo): ?string => $photo->getImageName())->toArray()));
+        $this->messageBus->dispatch(new CacheResolveMessage($entityInstance->getPhotos()->map(static fn ($photo): ?string => $photo->getImageName())->toArray()));
     }
 
     /**
      * @throws ExceptionInterface
      */
-    public function publish(
-        MessageBusInterface $messageBus,
-        AdminContext $context,
-        EntityManagerInterface $em,
-        UrlGeneratorInterface $urlGenerator
-    ): Response {
+    public function publish(AdminContext $context): Response {
         /** @var Gallery $entity */
         $entity = $context->getEntity()->getInstance();
 
         $entity->publish();
 
-        $em->flush();
+        $this->em->flush();
 
         if ($entity->getId()) {
-            $messageBus->dispatch(new GalleryNewsletterMessage($entity->getId(), NewsletterType::GALLERY_NEW));
+            $this->messageBus->dispatch(new GalleryNewsletterMessage($entity->getId(), NewsletterType::GALLERY_NEW));
         }
 
-        return $this->redirect($context->getReferrer() ?: $urlGenerator->generate(DashboardController::ROUTE));
+        return $this->redirect($context->getReferrer() ?: $this->urlGenerator->generate(DashboardController::ROUTE));
+    }
+
+    public function publishWithoutSendNewsletter(AdminContext $context): Response
+    {
+        /** @var Gallery $entity */
+        $entity = $context->getEntity()->getInstance();
+
+        $entity->publish();
+
+        $this->em->flush();
+
+        return $this->redirect($context->getReferrer() ?: $this->urlGenerator->generate(DashboardController::ROUTE));
     }
 }
