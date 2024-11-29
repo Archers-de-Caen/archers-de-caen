c7d8d526fc012c4562dea1c1217e511e67f86122
am dzaien <damien-1998@live.fr>
2024-11-29 12:01:35 +0100

    fix(page): ingore pdf file for embedly

--- assets/js/embedly.js
+++ assets/js/embedly.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll( 'oembed[url]' ).forEach( element => {
+        // ignore pdfs
+        if (element.getAttribute('url').includes('.pdf')) {
+            return
+        }
+
         // Create the <a href="..." class="embedly-card"></a> element that Embedly uses
         // to discover the media.
         const anchor = document.createElement( 'a' )
